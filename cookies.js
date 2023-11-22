b3b42e410736351ff2a978b35fc9ef959c18fc21
Paulina <85867849+ppelka6@users.noreply.github.com>
2023-11-22 11:22:56 +0100

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,29 @@
+    function createCookie(name, value, days, path, domain, secure) {
+            if (days) {
+                var date = new Data();
+                data.setTime(date.getTime() + (days*24*60*60*1000));
+                var expires = da.toGMTString();
+            }
+            else var expires = "";
+            cookieString = name + "=" + escape (value);
+            if (expires) cookieString += "; expires=" + expires;
+            if (path) cookieString += "; path=" + escape (path);
+            if (domain) cookieString += "; domain=" + escape (domain);
+            if (secure) cookieString += "; secure"; document.cookie = cookieString;
+        }
+        
+        function getCookie(name) {
+            var nameEquals = name + "=";
+            var crumbs = document.cookie.split(';');
+            for (var i=0; i<crumb.length; i++) {
+                var crumb = crumbs[i].trim();
+                if (crumb.indexOf(nameEquals) == 0) {
+                    return unescape(crumb.substring(nameEquals.length, crumb.length));
+                }
+            }
+            return null;
+        }
+
+        function deleteCookie(name) {
+            createCookie(name,"",-1);
+        }
